84ad518f7760a2eab3ce4fe3629cffa524e89762
Taewoo Kim <127316769+KAITOKIDDA@users.noreply.github.com>
2026-04-13 07:48:20 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/java/com/codereferee/codereferee_server/CoderefereeServerApplication.java
@@ -0,0 +1,13 @@
+package com.codereferee.codereferee_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderefereeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderefereeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codereferee/codereferee_server/CoderefereeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codereferee.codereferee_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderefereeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
